4fecf27cd5a4f2fd14abb60fea5456576796daec
Huy Cuong <phanhuycuong2909@gmail.com>
2022-08-29 10:42:17 +0700

    first commit

--- app/src/main/java/com/myapp/app100/MainActivity.java
+++ app/src/main/java/com/myapp/app100/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // new commit
+        // new commit 2
         setContentView(R.layout.activity_main);
     }
 }
